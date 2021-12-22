c84cc16bdf4def80e953f9d250e70587625fa8cf
nediyadath@gmail.com <@Power123>
2021-12-22 21:33:53 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using MVCExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Product p = new Product();
+            p.id = 101;
+            p.name = "Oven";
+            p.desc = "Microwave";
+
+                return View(p);
+        }
+
+        public ActionResult FetchData()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
